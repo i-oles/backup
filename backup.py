1ff793a91e9c69913cd9d22a55b0de31a13a0651
Igor Oleś <ioles@igoroles.local>
2022-01-11 12:02:53 +0100

    final_version

--- backup.py
+++ backup.py
@@ -1,16 +1,37 @@
 import os
+import time
+from os.path import expanduser, getmtime
+import shutil
+import glob
+import pathlib
 
-directory = ''
+folder_name = 'your_folder_name'
 
-win_parent_dir = ''
-mac_parent_dir = ''
-rpi_parent_dir = ''
+backup_dir_path = os.path.join(expanduser("~"), 'Desktop', folder_name)
+source_dir_path = os.path.join(pathlib.Path.cwd(), folder_name)
 
+all_source_files = []
 
-def backup():
-    pass
+if os.path.exists(backup_dir_path):
+    for path, subdirs, files in os.walk(source_dir_path):
+        for name in files:
+            all_source_files.append(os.path.join(path, name))
 
+    for source_file in all_source_files:
+        source_path = pathlib.Path(source_file)
+        index = source_path.parts.index(folder_name)
+        backup_file = pathlib.Path(os.path.join(expanduser("~"), 'Desktop', *source_path.parts[index:]))
 
-
-if __name__ == "__main__":
-    backup()
+        if not os.path.exists(backup_file):
+            os.makedirs(os.path.dirname(backup_file), exist_ok=True)
+            shutil.copy(source_file, backup_file)
+            print(f'new file created: {source_file}')
+        else:
+            source_file_date = time.ctime(os.path.getmtime(source_file))
+            backup_file_date = time.ctime(os.path.getmtime(backup_file))
+            if source_file_date > backup_file_date:
+                os.makedirs(os.path.dirname(backup_file), exist_ok=True)
+                shutil.copy(source_file, backup_file)
+                print(f'updated file: {source_file}')
+else:
+    shutil.copytree(source_dir_path, backup_dir_path)
